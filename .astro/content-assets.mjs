b55d7b68b189c017e8c54a91cfbec940a53b06b2
Kunjanaphorn Boonmak <kunjanaphorn.b@gmail.com>
2025-07-28 21:11:05 +0700

    Update image url

--- .astro/content-assets.mjs
+++ .astro/content-assets.mjs
@@ -1,5 +1,5 @@
 
-import __ASTRO_IMAGE_IMPORT_lFXFy from "/src/images/blog/1/1.png?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2F2023-A-Year-In-Review.md";
-import __ASTRO_IMAGE_IMPORT_1g2IoG from "/src/images/blog/2/2.png?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2FRecap-Q1-2024.md";
-export default new Map([["/src/images/blog/1/1.png?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2F2023-A-Year-In-Review.md", __ASTRO_IMAGE_IMPORT_lFXFy], ["/src/images/blog/2/2.png?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2FRecap-Q1-2024.md", __ASTRO_IMAGE_IMPORT_1g2IoG]]);
+import __ASTRO_IMAGE_IMPORT_Z2x3Bqp from "/src/images/blog/1.png?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2F2023-A-Year-In-Review.md";
+import __ASTRO_IMAGE_IMPORT_Zmf4js from "/src/images/blog/2.png?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2FRecap-Q1-2024.md";
+export default new Map([["/src/images/blog/1.png?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2F2023-A-Year-In-Review.md", __ASTRO_IMAGE_IMPORT_Z2x3Bqp], ["/src/images/blog/2.png?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2FRecap-Q1-2024.md", __ASTRO_IMAGE_IMPORT_Zmf4js]]);
 		
